f7812f4b8934d6661a06acb33ecfc8f42d034c3c
Pavlo <oksanakopalko@gmail.com>
2024-12-16 21:45:58 +0200

    1

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,18 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+// eslint-disable-next-line import/no-cycle
+import { todos } from '../../App';
+
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = () => {
+  if (todos.length === 0) {
+    return null;
+  }
+
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} />
+      ))}
+    </section>
+  );
+};
